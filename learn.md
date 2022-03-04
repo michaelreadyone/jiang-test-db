35a1edf154402a05858b7dadd0e3c4f774a139d6
Mike <michael56653556@gmail.com>
2022-03-04 00:26:01 -0500

    Flask API Folder structure

---
+++ learn.md
@@ -0,0 +1,10 @@
+# Change didn't show in git
+
+## Flask API Folder structure
+
+write below code to let flask know where flask app is
+
+``` bash
+export FLASK_ENV=development
+export FLASK_APP=src
+```
---
+++ src/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import os
+
+
+def create_app(test_config=None):
+
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config is None:
+
+        app.config.from_mapping(
+            SECRET_KEY=os.environ.get("SECRET_KEY"),
+        )
+    else:
+        app.config.from_mapping(test_config)
+
+    @app.get("/")
+    def index():
+        return "Hello"
+
+    @app.get("/hello")
+    def say_hello():
+        return {"message": "Hello world"}
+
+    return app
